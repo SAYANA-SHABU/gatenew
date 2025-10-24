72d8d03d209fc1e3ed87844cb6e1449041bcfda0
SAYANA-SHABU <144515427+SAYANA-SHABU@users.noreply.github.com>
2025-10-24 20:59:14 +0530

    Update student.js

--- project/backend/models/student.js
+++ project/backend/models/student.js
@@ -1,19 +1,23 @@
+// student.js
 var mongoose = require('mongoose');
+
 const studentSchema = new mongoose.Schema({
-   name:String,
-    admNo:Number,
-    dept:String,
-    sem:Number,
-    tutorName:String,
-    email:String,
-    image:Buffer,
-    parent_No:Number,
-    password:String,
-    phone:Number,
-    purpose: String,
-    date:Date,
-    returnTime: String
-    
+   name: String,
+   admNo: Number,
+   dept: String,
+   sem: Number,
+   tutorName: String,
+   email: String,
+   image: Buffer,
+   parent_No: Number,
+   password: String,
+   phone: Number,
+   purpose: String,
+   date: Date,
+   returnTime: String,
+   verified: { type: Boolean, default: false },
+   groupId: { type: mongoose.Schema.Types.ObjectId, ref: 'GatePass', default: null } // <-- new
 });
+
 const Student = mongoose.model('Student', studentSchema);
 module.exports = Student;
